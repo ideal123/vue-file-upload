d6fb273b780b3ac21a95cc11bc8f821bc0f722e9
ideal123 <iseeucome@outlook.com>
2017-11-27 21:12:24 +0800

    完成选择文件功能

---
+++ src/file-upload/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="file-upload">
+    <input class="file-upload-input" type="file" name="file" ref="input" @change="handleChange">
+    <button class="file-upload-btn" @click="$refs.input.click()">选择文件</button>
+  </div>
+</template>
+
+<script>
+import upload from './upload'
+export default {
+  methods: {
+    handleChange (e) {
+      const files = e.target.files
+
+      if (!files) {
+        return
+      }
+
+      upload(files)
+    }
+  }
+}
+</script>
+
+<style>
+.file-upload {
+  display: inline-block;
+}
+.file-upload .file-upload-input {
+  display: none;
+}
+.file-upload .file-upload-btn {
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  outline: none;
+  background-color: white;
+  cursor: pointer;
+  transition: .3s border-color, background-color;
+}
+.file-upload .file-upload-btn:hover,
+.file-upload .file-upload-btn:active {
+  border-color: #adadad;
+  background-color: #e6e6e6;
+}
+</style>
+
+
---
+++ src/file-upload/upload.js
@@ -0,0 +1,3 @@
+export default function (files) {
+  alert('upload TODO')
+}
